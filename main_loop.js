8a78d91b6a9c88fe6e43d9e184bd21fa44b0fcd5
Diego Auza <auzajuan1@gmail.com>
2024-01-30 22:08:41 -0500

    adding negative even numbers

--- main_loop.js
+++ main_loop.js
@@ -1,7 +1,7 @@
 // Write a foorlopp that will console.log number from 101 to 41 
 // a. Then convert this loop to print only the even number from 101 - 41.
 
-let initalValue = 41;
+let initalValue = 120;
 let finalValue = 101;
 let instructionAll = "all";
 let instructionEven = "even";
@@ -10,31 +10,53 @@ let instructionOdd = "odd";
 
 function easyForLoop(initalValueLoop, finalValueLoop,instruction ){
 
-    //
-    if(initalValueLoop <= finalValueLoop){
+    // no number because there is not instruction 
+    if(initalValueLoop <= finalValueLoop &&  instruction == undefined){
         console.log(`the final value must be larger than the inital value, inital value : ${initalValueLoop}, final ${finalValueLoop}`)
     } 
     //
-    //
+    // all number 
     //
     if(initalValueLoop < finalValueLoop && instruction == "all"){
-        console.log("------------------ counter all --------------");
+        console.log("------------------ counter all  --------------");
         for(let index = initalValue; index <=  finalValueLoop; index++){
-             console.log(`|                   ${index }                      |`);
+            console.log(`|                    ${index }                       |`);
         }
-        console.log("------------------ counter all -----------------");
+        console.log("------------------ counter all   -----------------");
     }
     //
+    // all number but it is going backwards
     //
+    if(initalValueLoop > finalValueLoop && instruction == "all"){
+        console.log("------------------  counter all (-) --------------");
+        for(let index = initalValueLoop; index >=  finalValueLoop; index--){
+            console.log(`|                   -${index }                      |`);
+        }
+        console.log("------------------ counter all (-) -----------------");
+    }
+    //
+    // all even numbers
     //
     if(initalValueLoop < finalValueLoop && instruction == "even"){
         console.log("------------------   even    --------------");
         for(let index = initalValue; index <=  finalValueLoop; index++){
             if( index % 2 == 0){
-        console.log(`|               ${index}                  |`);
+                console.log(`|               ${index}                  |`);
             }
         }
         console.log("------------------   even    --------------");
+    }
+    //
+    //
+    //
+    if(initalValueLoop > finalValueLoop && instruction == "even"){
+        console.log("------------------  counter all (-) --------------");
+        for(let index = initalValueLoop; index >=  finalValueLoop; index--){
+            if(index % 2 == 0){
+                console.log(`|                   -${index }                      |`);
+            }
+        }
+        console.log("------------------ counter all (-) -----------------");
     }
     //
     //
@@ -43,7 +65,7 @@ function easyForLoop(initalValueLoop, finalValueLoop,instruction ){
         console.log("------------------ odd --------------");
         for(let index = initalValue; index <=  finalValueLoop; index++){
             if( index % 2 != 0){
-        console.log(`|               ${index}                  |`);
+                console.log(`|               ${index}                  |`);
             }
         }
         console.log("------------------ odd  --------------");
@@ -52,4 +74,4 @@ function easyForLoop(initalValueLoop, finalValueLoop,instruction ){
 
 }
 
-easyForLoop(initalValue, finalValue, instructionOdd);
+easyForLoop(initalValue, finalValue, instructionEven);
