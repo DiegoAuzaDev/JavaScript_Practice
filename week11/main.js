987076f63551c6f960a7170e20d5e7d5b8bbcff3
auza0002 <auzajuan1@gmail.com>
2024-03-18 18:19:45 -0400

    url img

--- week11/main.js
+++ week11/main.js
@@ -8,10 +8,23 @@ function init() {
     return response.blob()
   }).then(data=>{
     console.log(data)
+    console.log(URL.createObjectURL(data))
+    let imageUrl = URL.createObjectURL(data)
+    displayImage(imageUrl)
+
+
   })
   .catch((err)=>{
     console.log(err)
   })
+}
+
+function displayImage(imgUrl){
+
+    let myImage = document.createElement("img")
+    myImage.setAttribute("src", imgUrl);
+    document.querySelector("main").append(myImage)
+
 }
 
 document.addEventListener("DOMContentLoaded", init);
